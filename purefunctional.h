6028fe4d4981338e7dbec7890b86c3ccf928f870
Hectarea <hectometrocuadrado@gmail.com>
2019-11-24 12:34:00 +0100

    Update purefunctional.h

--- purefunctional.h
+++ purefunctional.h
@@ -2,7 +2,9 @@
 #define PUREFUNCTIONAL_H_INCLUDED
 
 #include "bool.h"
-#include "integer.h"
-//#include "tuple.h"
+#include "numeric.h"
+#include "list.h"
+#include "eq.h"
+#include "ord.h"
 
 #endif // PUREFUNCTIONAL_H_INCLUDED
